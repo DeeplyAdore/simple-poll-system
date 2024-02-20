9f8f634766caf50b020210df97b30fd39f73a0cc
DeeplyAdore <47216867+DeeplyAdore@users.noreply.github.com>
2024-02-20 04:44:16 -0600

    Update APIService.ts

--- src/services/APIService.ts
+++ src/services/APIService.ts
@@ -1,20 +1,8 @@
-import { IItem } from '@/models/item';
-import { ICount } from '@/models/count';
 import { IPoll } from '@/models/poll';
-import { IListItem } from '@/models/list-item';
 
 export class APIService {
-  public getPollCount = async (item: IItem, pollName: string): Promise<ICount[]> =>
-    (await (await fetch(`/api/poll/${pollName}/${item.id}?type=count`)).json()).filter(
-      (p: ICount) => item.options.includes(p.Value)
-    );
 
-  public getPollList = async (pollName: string): Promise<IListItem[]> => {
-    const res = await (await fetch(`/api/poll/${pollName}?type=list`)).json();
-    return res.results;
-  };
-
-  public addPoll = (poll: IPoll): Promise<Response> =>
+  public addPoll = (poll: IPoll): Promise<Response> =
     fetch(`/api/poll/${poll.pollName}`, {
       method: 'POST',
       headers: {
