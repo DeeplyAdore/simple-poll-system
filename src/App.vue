8e754e4541c644bb6eaa32a37ff222a2088d6081
DeeplyAdore <47216867+DeeplyAdore@users.noreply.github.com>
2024-02-20 22:24:02 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,6 @@
 
 <script setup lang="ts">
 import Form from '@/components/form/Form.vue';
-import { ref } from 'vue';
-import { IItem } from '@/models/item';
-
-const templateItems: IItem[] = [
-  {
-    id: 'q1',
-    options: ['Cats', 'Dogs', 'Birds']
-  }
-];
-const items = ref(templateItems);
 </script>
 
 <style scoped></style>
